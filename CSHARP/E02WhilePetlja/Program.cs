9847c784d6fe6a586b68a7fae635c74df6f059ee
Darko Kučan <131719007+dkucan@users.noreply.github.com>
2023-07-05 23:38:43 +0200

    vježbanje

---
+++ CSHARP/E02WhilePetlja/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+//while radi s bool tipom podataka
+
+//beskonačna petlja
+while(true)
+{
+    break;
+}
+
+//u for se ne mora ući
+for(int i = 0; i < 0; i++)
+{
+    Console.WriteLine("01 Nisam ušao");
+}
+
+// u while se ne mora ući
+
+while(false)
+{
+    Console.WriteLine("02 Nisam ušao");
+}
+
+
+int b = 0;
+
+while (b<10)
+{
+    Console.WriteLine(++b);
+}
+
+int t = 2;
+b = 0;
+while (t==2 && b<10)
+{
+    Console.WriteLine(++b);
+}
+Console.WriteLine("*******************");
+b = 0;
+while (true)
+{
+    if (b == 2)
+    {
+        b++;
+        continue;
+    }
+    if (b == 5) {
+        break;
+    }
+    Console.WriteLine(b++);
+}
+
