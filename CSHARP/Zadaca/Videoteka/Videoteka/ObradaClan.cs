b1217004b7098e609ab5e1669ce0147a247b81a5
Darko Kučan <131719007+dkucan@users.noreply.github.com>
2023-07-18 07:45:05 +0200

    ljetni_zadatak
    Za sada ima još grešaka koje trebam ispraviti.

---
+++ CSHARP/Zadaca/Videoteka/Videoteka/ObradaClan.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Videoteka
+{ 
+  public class ObradaClan
+{
+    public List<clan> Clan { get; }
+
+    public ObradaClan()
+    {
+        Clan = new List<clan>();
+        if (Pomocno.dev)
+        {
+            TestniPodaci();
+        }
+    }
+
+    public void PrikaziIzbornik()
+    {
+        Console.WriteLine("Izbornik za rad s clanovima");
+        Console.WriteLine("1. Pregled postojećih clanova");
+        Console.WriteLine("2. Unos novog clana");
+        Console.WriteLine("3. Promjena postojećeg clana");
+        Console.WriteLine("4. Brisanje clana");
+        Console.WriteLine("5. Povratak na glavni izbornik");
+        switch (Pomocno.ucitajBrojRaspon("Odaberite stavku izbornika clanovi: ",
+            "Odabir mora biti 1-5", 1, 5))
+        {
+
+            case 1:
+                PregledClanova();
+                PrikaziIzbornik();
+                break;
+            case 2:
+                UcitajClana();
+                PrikaziIzbornik();
+                break;
+            case 3:
+                PromjenaClana();
+                PrikaziIzbornik();
+                break;
+            case 4:
+                BrisanjeClana();
+                PrikaziIzbornik();
+                break;
+            case 5:
+                Console.WriteLine("Gotov rad s clanovima");
+                break;
+
+
+        }
+    }
+
+    private void PromjenaClanova()
+    {
+        PregledClana();
+        int index = Pomocno.ucitajBrojRaspon("Odaberi redni broj clana: ", "Nije dobar odabir", 1, Clanovi.Count());
+        var p = Clanovi[index - 1];
+        p.Sifra = Pomocno.ucitajCijeliBroj("Unesite šifru clana (" + p.Sifra + "): ",
+            "Unos mora biti pozitivni cijeli broj");
+        p.Ime = Pomocno.UcitajString("Unesi ime clana (" + p.Ime + "): ", "Ime obavezno");
+        p.Prezime = Pomocno.UcitajString("Unesi Prezime clana (" + p.Prezime + "): ", "Prezime obavezno");
+        p.Email = Pomocno.UcitajString("Unesi Email clana (" + p.Email + "): ", "Email obavezno");
+        p.Oib = Pomocno.UcitajString("Unesi OIB clana (" + p.Oib + "): ", "OIB obavezno");
+    }
+
+    private void BrisanjeClana()
+    {
+        PregledClanova();
+        int index = Pomocno.ucitajBrojRaspon("Odaberi redni broj clana: ", "Nije dobar odabir", 1, Clanovi.Count());
+        Clanovi.RemoveAt(index - 1);
+    }
+
+    public void PregledClanova()
+    {
+        Console.WriteLine("------------------");
+        Console.WriteLine("---- Clanovi ----");
+        Console.WriteLine("------------------");
+        int b = 1;
+        foreach (clan clan in Clanovi)
+        {
+            Console.WriteLine("{0}. {1}", b++, clan);
+        }
+        Console.WriteLine("------------------");
+    }
+
+    private void UcitajClana()
+    {
+        var p = new clan();
+        p.Sifra = Pomocno.ucitajCijeliBroj("Unesite šifra clana: ",
+            "Unos mora biti pozitivni cijeli broj");
+        p.Ime = Pomocno.UcitajString("Unesi ime clana: ", "Ime obavezno");
+        p.Prezime = Pomocno.UcitajString("Unesi Prezime clana: ", "Prezime obavezno");
+        p.Email = Pomocno.UcitajString("Unesi Email clana: ", "Email obavezno");
+        p.Oib = Pomocno.UcitajString("Unesi OIB clana: ", "OIB obavezno");
+        Clanovi.Add(p);
+
+    }
+
+    private void TestniPodaci()
+    {
+        Clanovi.Add(new Clan
+        {
+            Sifra = 1,
+            Ime = "Ana",
+            Prezime = "Gal",
+            Email = "agal@gmail.com",
+            Oib = "33736472822"
+        });
+
+        Clanovi.Add(new Clan
+        {
+            Sifra = 2,
+            Ime = "Marija",
+            Prezime = "Zimska",
+            Email = "mzimska@gmail.com",
+            Oib = "33736472821"
+        });
+    }
+}
+}
---
+++ CSHARP/Zadaca/Videoteka/Videoteka/ObradaKazeta.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Videoteka
+
+ public class ObradaKazeta
+{
+    public List<Kazeta> Kazete{ get; }
+
+    public ObradaKazeta()
+    {
+        Kazete = new List<Kazeta>();
+        if (Pomocno.dev)
+        {
+            TestniPodaci();
+        }
+
+
+    }
+
+    public void PrikaziIzbornik()
+    {
+        Console.WriteLine("Izbornik za rad s kazetama");
+        Console.WriteLine("1. Pregled postojećih kazeta");
+        Console.WriteLine("2. Unos nove kazete");
+        Console.WriteLine("3. Promjena postojećih podataka o kazeti");
+        Console.WriteLine("4. Brisanje kazete");
+        Console.WriteLine("5. Povratak na glavni izbornik");
+        switch (Pomocno.ucitajBrojRaspon("Odaberite stavku izbornika Kazete: ",
+            "Odabir mora biti 1-5", 1, 5))
+        {
+            case 1:
+                PrikaziKazete();
+                PrikaziIzbornik();
+                break;
+            case 2:
+                UnosNoveKazete();
+                PrikaziIzbornik();
+                break;
+            case 3:
+                PromjenaKazete();
+                PrikaziIzbornik();
+                break;
+            case 4:
+                BrisanjeKazete();
+                PrikaziIzbornik();
+                break;
+            case 5:
+                Console.WriteLine("Gotov rad s kazetama");
+                break;
+        }
+    }
+
+    private void PromjenaKazete()
+    {
+        PrikaziSmjerove();
+        int index = Pomocno.ucitajBrojRaspon("Odaberi redni broj smjera: ", "Nije dobar odabir", 1, Kazeta.Count());
+        var s = Kazete[index - 1];
+        s.Sifra = Pomocno.ucitajCijeliBroj("Unesite šifru kazete (" + s.Sifra + "): ",
+            "Unos mora biti pozitivni cijeli broj");
+        s.Naziv = Pomocno.UcitajString("Unesite naziv kazete (" + s.Naziv + "): ",
+            "Unos obavezan");
+        s.Trajanje = Pomocno.ucitajCijeliBroj("Unesite trajanje kazete u satima (" + s.Trajanje + "): ",
+            "Unos mora biti cijeli pozitivni broj");
+        s.Cijena = Pomocno.ucitajDecimalniBroj("Unesite cijenu (. za decimalni dio) (" + s.Cijena + "): ", "Unos mora biti pozitivan broj");
+    }
+
+    private void BrisanjeKazete()
+    {
+        PrikaziSmjerove();
+        int index = Pomocno.ucitajBrojRaspon("Odaberi redni broj smjera: ", "Nije dobar odabir", 1, Kazeta.Count());
+        Kazeta.RemoveAt(index - 1);
+    }
+
+    private void UnosNovogSmjera()
+    {
+        var s = new Kazeta();
+        s.Sifra = Pomocno.ucitajCijeliBroj("Unesite šifra kazete: ",
+            "Unos mora biti pozitivni cijeli broj");
+        s.Naziv = Pomocno.UcitajString("Unesite naziv kazete: ",
+            "Unos obavezan");
+        s.Trajanje = Pomocno.ucitajCijeliBroj("Unesite trajanje kazete u satima: ",
+            "Unos mora biti cijeli pozitivni broj");
+        s.Cijena = Pomocno.ucitajDecimalniBroj("Unesite cijenu (. za decimalni dio): ", "Unos mora biti pozitivan broj");
+        Kazeta.Add(s);
+
+    }
+
+    public void PrikaziKazete()
+    {
+        Console.WriteLine("------------------");
+        Console.WriteLine("---- Kazete ----");
+        Console.WriteLine("------------------");
+        int b = 1;
+        foreach (Kazeta Kazeta in Kazete)
+        {
+            Console.WriteLine("{0}. {1}", b++, Kazeta.Naziv);
+        }
+        Console.WriteLine("------------------");
+    }
+
+    private void TestniPodaci()
+    {
+        Kazeta.Add(new Kazeta
+        {
+            Sifra = 1,
+            Naziv = "Web programiranje",
+            Trajanje = 250,
+            Cijena = 1000,
+        });
+
+        Kazeta.Add(new Kazeta
+        {
+            Sifra = 2,
+            Naziv = "Java programiranje",
+            Trajanje = 130,
+            Cijena = 1000,
+        });
+    }
+}
+}
