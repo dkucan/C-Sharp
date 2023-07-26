22c26e9845e06fbabf9668cef17f19760a6707e1
Darko Kučan <131719007+dkucan@users.noreply.github.com>
2023-07-26 21:39:17 +0200

    vježbanje, predavanja

---
+++ CSHARP/Console06/Grupa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E01KlasaObjekt
+{
+    internal class Grupa
+    {
+        public Grupa() 
+        {    
+            Console.WriteLine("Konstuktor Grupa");
+            }
+    }
+}
---
+++ CSHARP/Console06/Osoba.cs
@@ -0,0 +1,29 @@
+﻿namespace E01KlasaObjekt
+{
+    // Klasa je opisnik objekta
+    internal class Osoba
+    {
+        //ovako se ne smiju deklarirati svojstva u klasi
+        // zato što nije zadovoljen OOP princip učahurivanja
+        /* 
+         public string ime;
+        internal string ime;
+        int godine;
+        */
+
+        // zadnja vrsta metoda: konstruktor
+        // poziva se u trenutku instanciranja novog objekta (ključna riječ new)
+        // ona nije obavezna. Ako nije definirana poziva se konstruktor iz nadklase (nasljeđivanje)
+        // Naziv konstuktora mora biti identičan  nazivu klase
+
+        public Osoba()
+        {
+            Console.WriteLine("Konstruktor klase Osoba");
+        }
+
+        public Osoba (string ime)
+        {
+            Console.WriteLine(ime);
+        }
+    }
+}
