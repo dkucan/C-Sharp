3f542d258ed3cdfeb2840d83442e5450c7e7439c
Darko Kučan <131719007+dkucan@users.noreply.github.com>
2023-08-27 23:30:07 +0200

    Matrica - dorada

--- CSHARP/Zadaca/Matrica/Matrica/ConsoleApp1/Program.cs
+++ CSHARP/Zadaca/Matrica/Matrica/ConsoleApp1/Program.cs
@@ -7,87 +7,41 @@ int stupci = int.Parse(Console.ReadLine());
 
 int[,] matrica = new int[retci, stupci];
 
-
 int b = 1;
-int k = 0; 
+int k = 0;
 
-
-while (b < stupci * retci)
+while (b <= stupci * retci)
 {
-    for (int i = k+1; i <= stupci-k; i++)
-
+    for (int i = k; i < stupci - k; i++)
     {
-        matrica[retci - 1, stupci - i] = b++;
-        
+        matrica[retci - k - 1, i] = b++;
     }
 
-    // // i = 1 - matrica [5,5]
-    // // i = 2 - matrica [5,4]
-    // // i = 3 - matrica [5,3]
-    // // i = 4 - matrica [5,2]
-    // // i = 5 - matrica [5,1]
-    // // i = 6 - matrica [5,0]
-
-    for (int i = 0; i < retci-k; i++)
+    for (int i = retci - k - 2; i >= k; i--)
     {
-        for (int j = 0; j < stupci; j++)
-        {
-            Console.Write(matrica[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine("******************************");
-
-    for (int i = retci - k; i >= 0; i--)
-    {
-        matrica[i, 0] = b++;
-
-        // i = 4 - matrica [4,0]
-        // i = 3 - matrica [3,0]
-        // i = 2 - matrica [2,0]
-        // i = 1 - matrica [1,0]
-        // i = 0 - matrica [0,0]
+        matrica[i, k] = b++;
     }
 
-    for (int i = 0; i < retci-k; i++)
+    for (int i = k + 1; i < retci - k; i++)
     {
-        for (int j = 0; j < stupci; j++)
-        {
-            Console.Write(matrica[i, j] + "");
-        }
-        Console.WriteLine();
+        matrica[k, i] = b++;
     }
 
-    Console.WriteLine("******************************");
-    for (int i = 1; i < stupci-k; i++)
+    for (int i = stupci - k - 2; i > k; i--)
     {
-        matrica[0, i] = b++;
-       
+        matrica[i, stupci - k - 1] = b++;
     }
-
-    for (int i = 0; i < retci-k; i++)
-    {
-        for (int j = 0; j < stupci; j++)
-        {
-            Console.Write(matrica[i, j] + "");
-        }
-        Console.WriteLine();
-    }
-
-    Console.WriteLine("******************************");
-    for (int i = 1; i <= retci -k; i++)
-    {
-        matrica[i, stupci - 1] = b++;
-
-    }
-    for (int i = 0; i < retci; i++)
-    {
-        for (int j = 0; j < stupci; j++)
-        {
-            Console.Write(matrica[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-
+    k++;
 }
+
+// Ispis matrice
+for (int i = 0; i < retci; i++)
+{
+    for (int j = 0; j < stupci; j++)
+    {
+        Console.Write(matrica[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
 
