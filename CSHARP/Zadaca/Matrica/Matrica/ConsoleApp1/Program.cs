30492f8393709633cca22c240d28fc6b1888a413
Darko Kučan <131719007+dkucan@users.noreply.github.com>
2023-08-28 20:29:40 +0200

    Matrica - ispravljeni nedostaci

--- CSHARP/Zadaca/Matrica/Matrica/ConsoleApp1/Program.cs
+++ CSHARP/Zadaca/Matrica/Matrica/ConsoleApp1/Program.cs
@@ -14,22 +14,22 @@ while (b <= stupci * retci)
 {
     for (int i = k; i < stupci - k; i++)
     {
-        matrica[retci - k - 1, i] = b++;
-    }
-
-    for (int i = retci - k - 2; i >= k; i--)
-    {
-        matrica[i, k] = b++;
-    }
-
-    for (int i = k + 1; i < retci - k; i++)
-    {
         matrica[k, i] = b++;
     }
 
-    for (int i = stupci - k - 2; i > k; i--)
+    for (int i = k + 1; i <retci-k; i++)
     {
-        matrica[i, stupci - k - 1] = b++;
+        matrica[i, stupci-k-1] = b++;
+    }
+
+    for (int i = stupci - k - 2; i >=k; i--)
+    {
+        matrica [retci - k -1,i] = b++;
+    }
+
+    for (int i = retci - k - 2; i>k; i --)
+    {
+        matrica[i,k] = b++;
     }
     k++;
 }
