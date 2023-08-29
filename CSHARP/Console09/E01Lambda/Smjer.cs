c5cf7cd25b9404815abbf354ee420de9f88bf06d
Darko Kučan <131719007+dkucan@users.noreply.github.com>
2023-08-29 21:39:21 +0200

    Lambda izrazi - praćenje predavanja

---
+++ CSHARP/Console09/E01Lambda/Smjer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E01Lambda
+{
+    internal class Smjer
+    {
+
+        public int Sifra { get; set; }
+
+        //? označava kako Naziv može biti null
+        public string? Naziv { get; set; }
+
+        public override string ToString()
+        {
+            return Naziv ?? ""; //Ova linija je ekvivalent gornjoj
+        }
+    }
+
+}
--- CSHARP/Zadaca/Matrica/Matrica/ConsoleApp1/Program.cs
+++ CSHARP/Zadaca/Matrica/Matrica/ConsoleApp1/Program.cs
@@ -16,24 +16,20 @@ while (b <= stupci * retci)
     {
         matrica[k, i] = b++;
     }
-
     for (int i = k + 1; i <retci-k; i++)
     {
         matrica[i, stupci-k-1] = b++;
     }
-
     for (int i = stupci - k - 2; i >=k; i--)
     {
         matrica [retci - k -1,i] = b++;
     }
-
     for (int i = retci - k - 2; i>k; i --)
     {
         matrica[i,k] = b++;
     }
     k++;
 }
-
 // Ispis matrice
 for (int i = 0; i < retci; i++)
 {
