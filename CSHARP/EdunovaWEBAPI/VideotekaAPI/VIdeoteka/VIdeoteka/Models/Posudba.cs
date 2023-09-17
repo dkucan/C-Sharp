a50fac68f829092cb7f55198c0b44c2e91ac064c
Darko Kučan <131719007+dkucan@users.noreply.github.com>
2023-09-17 20:43:00 +0200

    get posudba radi

--- CSHARP/EdunovaWEBAPI/VideotekaAPI/VIdeoteka/VIdeoteka/Models/Posudba.cs
+++ CSHARP/EdunovaWEBAPI/VideotekaAPI/VIdeoteka/VIdeoteka/Models/Posudba.cs
@@ -6,11 +6,9 @@ namespace VIdeoteka.Models
     public class Posudba : Entitet
     {
 
-       
-        [Key]
-        public clan? Clan { get; set; }
         [ForeignKey("clan")]
-        public List <KAZETA> Kazete { get; set; }
+        public clan? Clan { get; set; }
+        public List<KAZETA> Kazete { get; set; } = new();
         public DateTime? Datum_posudbe { get; set; }
         public DateTime? Datum_vracanja { get; set; }
         public int Zakasnina { get; set; }  
